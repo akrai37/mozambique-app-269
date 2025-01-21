a2d157e8ce7eb0297c1f5357ed62e30ff9535ddf
joshsj89 <joshsj89@gmail.com>
2025-01-20 18:27:29 -0800

    Adjust sizes for tablet

--- lib/view/home_card.dart
+++ lib/view/home_card.dart
@@ -13,8 +13,8 @@ class HomeCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 100,
-      width: 100,
+      height: 200,
+      width: 200,
       child: Container(
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(5),
@@ -30,14 +30,14 @@ class HomeCard extends StatelessWidget {
             Text(
               icon,
               style: TextStyle(
-                fontSize: 50,
+                fontSize: 100,
                 fontWeight: FontWeight.bold,
               ),
             ),
             Text(
               title,
               style: TextStyle(
-                fontSize: 9,
+                fontSize: 18,
                 fontWeight: FontWeight.bold,
               ),
             ),
--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -12,115 +12,114 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    'DIFF EDUCATION',
-                    style: TextStyle(
-                      fontSize: 10,
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xFFE84C3D),
-                    ),
+      body: Column(
+        // Column is also a layout widget. It takes a list of children and
+        // arranges them vertically. By default, it sizes itself to fit its
+        // children horizontally, and tries to be as tall as its parent.
+        //
+        // Column has various properties to control how it sizes itself and
+        // how it positions its children. Here we use mainAxisAlignment to
+        // center the children vertically; the main axis here is the vertical
+        // axis because Columns are vertical (the cross axis would be
+        // horizontal).
+        //
+        // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+        // action in the IDE, or press "p" in the console), to see the
+        // wireframe for each widget.
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              spacing: 10,
+              children: [
+                const Text(
+                  'DIFF EDUCATION',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFFE84C3D),
                   ),
-                  Expanded( // ensures the TextField takes up the remaining space
-                    child: TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Search',
-                        hintStyle: TextStyle(
-                          fontSize: 10,
-                          fontWeight: FontWeight.bold,
-                          color: const Color(0xFF95A5A5),
-                        ),
-                        prefixIcon: const Icon(
-                          Icons.search,
-                          color: Color(0xFF95A5A5),
-                        ),
-                      ),
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      shape: WidgetStateProperty.all(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(5),
-                          side: const BorderSide(
-                            color: Color(0xFF2D3E50),
-                          ),
-                        ),
-                      ),
-                    ),
-                    child: const Text(
-                      'Practice',
-                      style: TextStyle(
+                ),
+                Expanded( // ensures the TextField takes up the remaining space
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Search',
+                      hintStyle: TextStyle(
                         fontSize: 10,
                         fontWeight: FontWeight.bold,
-                        color: Color(0xFF2D3E50),
+                        color: const Color(0xFF95A5A5),
+                      ),
+                      prefixIcon: const Icon(
+                        Icons.search,
+                        color: Color(0xFF95A5A5),
                       ),
                     ),
-                  )
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 4.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start, // aligns the children to the start (left) of the row
-                children: [
-                  const Text(
-                    'Olá!',
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    shape: WidgetStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        side: const BorderSide(
+                          color: Color(0xFF2D3E50),
+                        ),
+                      ),
+                    ),
+                  ),
+                  child: const Text(
+                    'Practice',
                     style: TextStyle(
-                      fontSize: 33,
+                      fontSize: 20,
                       fontWeight: FontWeight.bold,
                       color: Color(0xFF2D3E50),
                     ),
                   ),
-                ],
-              ),
-            ),
-            Wrap( // replaces Row so that the children wrap to the next line if they don't fit
-              direction: Axis.horizontal,
-              spacing: 10,
-              runSpacing: 10,
-              children: [
-                HomeCard(icon: '🔢', title: 'Números'),
-                HomeCard(icon: '🎨', title: 'Cores'),
-                HomeCard(icon: '🍎', title: 'Frutas'),
-                HomeCard(icon: '🥬', title: 'Produtos Hortícolas',),
-                HomeCard(icon: '🐾', title: 'Animais'),
-                HomeCard(icon: '🏃🏾', title: 'Ações Comuns'),
-                HomeCard(icon: '🗓️', title: 'Atividades Cotidianas'),
-                HomeCard(icon: '💼', title: 'Profissões'),
-                HomeCard(icon: '🏠', title: 'Coisas da Casa'),
-                HomeCard(icon: '🍽️', title: 'Coisas da Cozinha'),
-                HomeCard(icon: '🧑🏾', title: 'Rosto'),
-                HomeCard(icon: '🧍🏾', title: 'Corpo'),
-                HomeCard(icon: '👋🏾', title: 'Saudações'),
-                HomeCard(icon: '❓', title: 'Pedidos'),
+                )
               ],
             ),
-          ],
-        ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 90.0, vertical: 4.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start, // aligns the children to the start (left) of the row
+              children: [
+                const Text(
+                  'Olá!',
+                  style: TextStyle(
+                    fontSize: 33,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF2D3E50),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Wrap( // replaces Row so that the children wrap to the next line if they don't fit
+            direction: Axis.horizontal,
+            spacing: 10,
+            runSpacing: 10,
+            children: [
+              HomeCard(icon: '🔢', title: 'Números'),
+              HomeCard(icon: '🎨', title: 'Cores'),
+              HomeCard(icon: '🍎', title: 'Frutas'),
+              HomeCard(icon: '🥬', title: 'Produtos Hortícolas',),
+              HomeCard(icon: '🐾', title: 'Animais'),
+              HomeCard(icon: '🏃🏾', title: 'Ações Comuns'),
+              HomeCard(icon: '🗓️', title: 'Atividades Cotidianas'),
+              HomeCard(icon: '💼', title: 'Profissões'),
+              HomeCard(icon: '🏠', title: 'Coisas da Casa'),
+              HomeCard(icon: '🍽️', title: 'Coisas da Cozinha'),
+              HomeCard(icon: '🧑🏾', title: 'Rosto'),
+              HomeCard(icon: '🧍🏾', title: 'Corpo'),
+              HomeCard(icon: '👋🏾', title: 'Saudações'),
+              HomeCard(icon: '❓', title: 'Pedidos'),
+            ],
+          ),
+        ],
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
