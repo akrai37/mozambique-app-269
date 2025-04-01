34a37449ae42f1d4d688d86a159422fac130fcbc
joshsj89 <joshsj89@gmail.com>
2025-03-31 19:09:35 -0700

    Add speaker button to Learn Cards

--- lib/view/learn_card.dart
+++ lib/view/learn_card.dart
@@ -24,23 +24,54 @@ class LearnCard extends StatelessWidget {
             borderRadius: BorderRadius.circular(5),
             color: const Color(0xFFECF0F1),
           ),
-          child: Column(
+          child: Stack(
             children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Image(
-                  image: AssetImage(img.isNotEmpty ? img: 'assets/images/learn/Numbers.png'),
-                  width: 200,
-                  height: 200,
+              Positioned.fill(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image(
+                        image: AssetImage(img.isNotEmpty ? img : 'assets/images/learn/Numbers.png'),
+                        width: 200,
+                        height: 200,
+                      ),
+                    ),
+                    Text(
+                      title,
+                      style: const TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
                 ),
               ),
-              Text(
-                title,
-                style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
+
+              // Speaker Icon
+              Positioned(
+                bottom: 8,
+                right: 8,
+                child: InkWell(
+                  onTap: () {
+                    print('Speaker icon tapped for $title');
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.black.withValues( alpha: 0.3),
+                    ),
+                    padding: const EdgeInsets.all(10),
+                    child: const Icon(
+                      Icons.volume_up,
+                      color: Colors.white,
+                      size: 24,
+                    ),
+                  ),
+                )
+              )
             ],
           ),
         ),
