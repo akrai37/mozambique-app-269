df70c487b9a1e786dd7e7ceef1097f14284f18f1
Gwendolyn <52841252+gwynin@users.noreply.github.com>
2025-04-27 16:37:46 -0700

    import for screen lock

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:firebase_core/firebase_core.dart';
