7b5af1196c2f96f52487810c74fbba0bd6215899
joshsj89 <joshsj89@gmail.com>
2025-04-30 02:47:07 -0700

    Change practice mode colors of Navbar

--- lib/view/navbar.dart
+++ lib/view/navbar.dart
@@ -7,7 +7,7 @@ class Navbar extends StatefulWidget {
   final VoidCallback? onSync; // When the sync button is pressed
   final bool isHomeScreen; // To check if the user is on the HomeScreen
   final bool isInfoScreen; // To check if the user is on the InfoScreen
-  final bool isPractice;
+  final bool isPractice; // To check if the user is on a practice screen (dark mode)
   
   const Navbar({
     super.key,
@@ -57,23 +57,23 @@ class _NavbarState extends State<Navbar> {
           ),
           Expanded( // ensures the TextField takes up the remaining space
             child: TextField(
-              decoration: const InputDecoration(
+              decoration: InputDecoration(
                 hintText: 'Procurar...',
                 hintStyle: TextStyle(
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
-                  color: Color(0xFF95A5A5),
+                  color: widget.isPractice ? Colors.white : Color(0xFF95A5A5),
                 ),
                 prefixIcon: Icon(
                   Icons.search,
-                  color: Color(0xFF95A5A5),
+                  color: widget.isPractice ? Colors.white : Color(0xFF95A5A5),
                 ),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.all(Radius.circular(5)),
                   borderSide: BorderSide.none,
                 ),
                 filled: true,
-                fillColor: Color(0xFFECF0F1),
+                fillColor: widget.isPractice ? Color(0xFF969FA7) : Color(0xFFECF0F1),
               ),
               style: const TextStyle(
                 fontSize: 20,
