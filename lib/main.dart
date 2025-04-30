917b6361fdb2e4a5e1087be5728853fadf0261c4
joshsj89 <joshsj89@gmail.com>
2025-04-30 06:26:30 -0700

    Revert main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hive/hive.dart';
-import 'package:mozambique_app/view/home_screen.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:mozambique_app/firebase_options.dart';
 
-import 'package:mozambique_app/view/prac_convo.dart';
+import 'package:mozambique_app/view/home_screen.dart';
 import 'package:mozambique_app/model/conversation.dart';
 import 'package:mozambique_app/model/home_word.dart';
 import 'package:mozambique_app/model/question.dart';
@@ -92,14 +91,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    //return PracConvo(title: "Saudações", tag: "Greetings");
-    //return PracConvo(title: "Pedidos", tag: "requests");
-    return PracConvo(title: "Interações Pessoais", tag: "personalInteractions");
+    return HomeScreen();
   }
 }
