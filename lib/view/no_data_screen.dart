1d4b38e779ea0d288fdf56760e7d5636c0277599
alexlucaslopes <alex.lucas.lopes@gmail.com>
2025-04-30 22:25:51 -0700

    New no_data_screen

--- lib/view/no_data_screen.dart
+++ lib/view/no_data_screen.dart
@@ -3,18 +3,53 @@ import 'package:flutter/material.dart';
 class NoDataScreen extends StatelessWidget {
   const NoDataScreen({super.key});
 
-  @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(
-        child: Text(
-          "No content available.\nPlease connect to the internet and restart the app.",
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-            color: Colors.black54,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 90.0, vertical: 4.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start, // aligns the children to the start (left) of the row
+                    children: [
+                      Text(
+                        'Vamos Começar',
+                        style: TextStyle(
+                          fontSize: 50,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF2D3E50),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+          SizedBox(
+            child: SingleChildScrollView(
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 10),
+                  
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 10),
+                    child: Text(
+                      "Bem-vindo! Parece que você ainda não está conectado à internet. Este aplicativo precisa de uma conexão com a internet na primeira vez que você o abrir para que possamos carregar todo o conteúdo, images e recursos que tornam sua experiência completa. Para começar, conecte seu dispositivo ao Wi-Fi ou aos dados móveis e, em sequida, feche e abra o aplicativo novamente.",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black54,
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: 20),
+                ],
+              ),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
