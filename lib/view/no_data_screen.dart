2ccdf916058590abdc428122d587d8f9094d1ac1
joshsj89 <joshsj89@gmail.com>
2025-04-30 22:42:23 -0700

    Update no_data_screen.dart

--- lib/view/no_data_screen.dart
+++ lib/view/no_data_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class NoDataScreen extends StatelessWidget {
   const NoDataScreen({super.key});
 
+  @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Column(
