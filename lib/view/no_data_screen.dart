f7da0589b951242ce2327ceab32ca6bd7221032b
joshsj89 <joshsj89@gmail.com>
2025-05-01 18:13:21 -0700

    Add background color to No Connection Screen

--- lib/view/no_data_screen.dart
+++ lib/view/no_data_screen.dart
@@ -6,6 +6,7 @@ class NoDataScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      backgroundColor: Colors.white,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
